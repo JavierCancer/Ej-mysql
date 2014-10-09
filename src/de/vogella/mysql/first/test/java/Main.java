55139d94a1bff197cf5afd4e6565fe3b82d4c7f8
Javier <Javier@10.89.45.97>
2014-10-09 16:45:12 +0200

    Commit mysql

---
+++ src/de/vogella/mysql/first/test/java/Main.java
@@ -0,0 +1,9 @@
+package de.vogella.mysql.first.test.java;
+import de.vogella.mysql.first.jars.first;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+	  first dao = new first();
+    dao.readDataBase();
+  }
+} 
